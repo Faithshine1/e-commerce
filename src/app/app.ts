4abc70453be6a601cd741165ab2c0dbcf2c66026
minha pika <davi15mmartins@gmail.com>
2026-07-06 16:30:40 -0300

    feat: Adiciona o componente Produto para exibir informações do produto e remove template padrão do angular CLI

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+//!import { RouterOutlet } from '@angular/router'; //remove importação do RouterOutlet, pois não está sendo usado no momento
+import { Produto } from './components/produto/produto'; //Import do componente Produto
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [Produto],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
