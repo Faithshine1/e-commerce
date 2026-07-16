7b067070c07ab2510affccd3c780856aad2d2ec1
Davi Martins <davi15mmartins@gmail.com>
2026-07-16 14:16:29 -0300

    feat(pipes):adiciona pipe para formatação da moeda BRL no e-commerce

---
+++ src/app/features/produtos/lista-produtos/lista-produtos.ts
@@ -0,0 +1,39 @@
+import { Component } from '@angular/core';
+import {Produto } from '../produto/produto';
+@Component({
+  selector: 'app-lista-produtos',
+  imports: [Produto],
+  templateUrl: './lista-produtos.html',
+  styleUrl: './lista-produtos.css',
+})
+export class ListaProdutos {
+  produtos = [
+    { nome: 'Monitor Gamer',
+      preco: 199.99 },
+  
+    { nome: 'Smartphone', 
+      preco: 1599.99 },
+
+    { nome: 'Tablet', 
+      preco: 12.99 },
+
+    { nome: 'Notebook Gamer', 
+      preco: 2500.00 },
+    { nome: 'Teclado Mecânico', 
+      preco: 299.99 },
+    { nome: 'Mouse Gamer', 
+      preco: 149.99 },
+
+    { nome: 'Headset Gamer', 
+      preco: 89.99 },
+
+    { nome: 'Cadeira Gamer', 
+      preco: 499.99 },
+
+    { nome: 'Placa de Vídeo', 
+      preco: 1999.99 },
+  ];exibirProduto(nome: string) {
+    console.log('Produto selecionado: ', nome);
+  }
+}
+//adiciona lista de produtos com nome e preço, e exibe no console o produto selecionado.
