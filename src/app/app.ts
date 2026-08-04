6a81dd16dd83b1579fadc72e52ab4d81f15c0076
Davi Martins <davi15mmartins@gmail.com>
2026-08-04 17:05:37 -0300

    feat(app.ts): Adiciona novo import Header e MatAnchoer

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,17 +1,20 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet, RouterLink } from '@angular/router';
+import { RouterOutlet } from '@angular/router';
+import {RouterLink} from '@angular/router'
 import { usuarioLogado, login, logout } from './core/auth';
+import {Header} from './shared/layout/header/header'
+import { MatAnchor } from "@angular/material/button";
+import { UpperCasePipe } from '@angular/common';
 //!import { ListaProdutos } from './features/produtos/lista-produtos/lista-produtos';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterLink],
+  imports: [RouterOutlet, RouterLink, Header, MatAnchor],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('e-commerce');
-  nomeLoja = 'MARTINS TECH'; //nome do e-commerce
   usuarioLogado = usuarioLogado;
   login = login;
   logout = logout;
