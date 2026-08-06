55b318dfb5eeae98d460d4f7c27bc2d0b99e5175
Davi Martins <davi15mmartins@gmail.com>
2026-08-06 15:04:57 -0300

    feat: adiciona novo import

--- src/app/shared/layout/header/header.ts
+++ src/app/shared/layout/header/header.ts
@@ -1,15 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import {MatToolbarModule} from '@angular/material/toolbar'
 import {MatIconModule} from '@angular/material/icon'
 import { MatButtonModule } from '@angular/material/button';
 import {RouterLink} from '@angular/router';
 import { UpperCasePipe } from '@angular/common';
+import { CarrinhoService } from '../../../core/services/carrinho.service';
+
 @Component({
   selector: 'app-header',
-  imports: [MatToolbarModule, MatIconModule, MatButtonModule, RouterLink],
+  imports: [MatToolbarModule, MatButtonModule, MatIconModule, RouterLink, UpperCasePipe ],
   templateUrl: './header.html',
   styleUrl: './header.css',
 })
 export class Header {
   nomeLoja = 'MARTINS TECH';
+  public carrinhoService = inject(CarrinhoService);
+  quantidade = this.carrinhoService.quantidadeItens;
 }
