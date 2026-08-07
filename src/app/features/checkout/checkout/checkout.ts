5b5eaa214386940af78a7d3aa7be0c7f8eb4f6a9
Davi Martins <davi15mmartins@gmail.com>
2026-08-07 15:09:25 -0300

    feat(checkout.ts): Adiciona validador de informações do checkout.

--- src/app/features/checkout/checkout/checkout.ts
+++ src/app/features/checkout/checkout/checkout.ts
@@ -1,8 +1,10 @@
 import { Component, inject } from '@angular/core';
-import {Inject} from '@angular/core'
 import { ReactiveFormsModule } from '@angular/forms'
 import { FormGroup } from '@angular/forms'
 import { FormControl } from '@angular/forms';
+import { Validators} from '@angular/forms';
+import { AbstractControl } from '@angular/forms';
+import { ValidationErrors } from '@angular/forms';
 import { CarrinhoService } from '../../../core/services/carrinho.service';
 
 @Component({
@@ -15,13 +17,20 @@ export class Checkout {
   carrinhoService = inject(CarrinhoService);
 
   formulario = new FormGroup({
-    nome: new FormControl(''),
-    email: new FormControl(''),
-    endereco: new FormControl(''),
-    telefone: new FormControl(''),
+    nome: new FormControl('', [Validators.required, Validators.minLength(3), nomeSemNumeros]),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    endereco: new FormControl('', [Validators.required, Validators.minLength(5)]),
   });
   finalizar (){
     console.log('Dados do Formulário: ', this.formulario.value)
     console.log('Itens do Carrinho: ', this.carrinhoService.itens())
   }
+}
+function nomeSemNumeros(control: AbstractControl):ValidationErrors | null {
+  const valor= control.value;
+  if(!valor) return null;
+  if(/\d/.test(valor)){
+    return{nomeInvalido: true};
+  }
+  return null;
 }
