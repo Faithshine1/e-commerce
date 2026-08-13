a9330c5c85305455e1d04c92123d180967fbc89b
Davi Martins <davi15mmartins@gmail.com>
2026-08-13 17:02:10 -0300

    refactor(app.ts): retira os strings na export class

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,7 +1,6 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {RouterLink} from '@angular/router'
-import { usuarioLogado, login, logout } from './core/auth';
 import {Header} from './shared/layout/header/header'
 import { MatAnchor } from "@angular/material/button";
 //!import { ListaProdutos } from './features/produtos/lista-produtos/lista-produtos';
@@ -14,7 +13,6 @@ import { MatAnchor } from "@angular/material/button";
 })
 export class App {
   protected readonly title = signal('e-commerce');
-  usuarioLogado = usuarioLogado;
-  login = login;
-  logout = logout;
+  
+  
 }
