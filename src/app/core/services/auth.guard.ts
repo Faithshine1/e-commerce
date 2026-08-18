a6cd46a7cb19f5173b6a8ed62fb0d449c850f43c
Davi Martins <davi15mmartins@gmail.com>
2026-08-18 14:00:40 -0300

    refactor(auth.guard.ts): adiciona novo import AuthFacade e reestrutura o código mudando service para Facade

--- src/app/core/services/auth.guard.ts
+++ src/app/core/services/auth.guard.ts
@@ -1,17 +1,17 @@
 import { CanActivateFn } from "@angular/router";
 import { inject } from "@angular/core";
 import { Router } from "@angular/router";
-import { AuthService } from "./auth.service";
+import { AuthFacade } from "../facades/auth.facade";
 
 export const authGuard: CanActivateFn = () => {
-    const authService = inject(AuthService);
+    const authFacade = inject(AuthFacade);
     const router = inject(Router);
 
-    if(!authService.usuarioLogado()){
+    if(!authFacade.usuarioLogado()){
     return router.createUrlTree(['/login']);
 }
 //!
-if(!authService.admin()){
+if(!authFacade.admin()){
     return router.createUrlTree(['/acesso-negado']);
 }
 //!
