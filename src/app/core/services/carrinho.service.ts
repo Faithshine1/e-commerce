9c638d5a7b5c61fc7f3f71389684dad8ffcdca61
Davi Martins <davi15mmartins@gmail.com>
2026-08-19 16:06:11 -0300

    refactor(carrinho.service.ts): Reestrutura o código deixando a pessoa ficar no site mesmo sem conexão e deixar os itens do carrinho salvos.

--- src/app/core/services/carrinho.service.ts
+++ src/app/core/services/carrinho.service.ts
@@ -1,16 +1,18 @@
-import { Injectable } from "@angular/core";
-import { signal } from "@angular/core";
+import { Injectable, PLATFORM_ID} from "@angular/core";
+import { signal, effect, inject } from "@angular/core";
 import { computed } from "@angular/core";
 import { ItemCarrinho } from "../models/item-carrinho";
-
+import { isPlatformBrowser } from "@angular/common";
 
 @Injectable({
     providedIn:'root'
 })
 
 export class CarrinhoService{
+
+
 //! Estado global
-private carrinho = signal<ItemCarrinho[]>([]);
+private carrinho = signal<ItemCarrinho[]>(this.carregarcarrinhoSalvo());
 
 //? Seletores
 itens = computed(()=> this.carrinho());
@@ -18,7 +20,49 @@ quantidadeItens = computed(() => this.carrinho().length); //! Quantidade de Iten
 totalItens = computed (()=> 
      this.carrinho().reduce((total, item)=> total + item.preco, 0)
 );
+
 carrinhoVazio = computed(()=> this.carrinho().length === 0); //metodo
+
+
+//! =========== PERSISTÊNCIA DE DADOS DO CARRINHO ==============
+
+private platformId = inject(PLATFORM_ID);
+
+//! Chave de recuperação Localstorage
+private readonly chavesStorage = 'carrinho-storage';
+constructor(){
+    effect(()=>{
+    this.salvarCarrinho(this.carrinho());
+    });
+}
+
+private estaNoNavegador(): boolean{
+    return isPlatformBrowser(this.platformId);
+}
+
+private carregarcarrinhoSalvo(): ItemCarrinho []{
+        if(!this.estaNoNavegador()){
+            return [];
+    }
+    const dadosSalvos = localStorage.getItem(this.chavesStorage);
+
+    if (!dadosSalvos){
+        return[];
+    }
+    try{
+        return JSON.parse(dadosSalvos) as ItemCarrinho[];
+    } catch {
+        return[];
+    }
+}
+
+private salvarCarrinho (item: ItemCarrinho[]){
+    if(!this.estaNoNavegador){
+        return;
+    }
+
+localStorage.setItem(this.chavesStorage, JSON.stringify(item));
+}
 
 // TODO: Ações
 adicionar(produto: ItemCarrinho){
