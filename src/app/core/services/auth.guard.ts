afff89024f5faf3e823a66532d4a7c3d5859ad60
Davi Martins <davi15mmartins@gmail.com>
2026-08-19 14:12:34 -0300

    chore(auth.guard.ts): novo import e funcionalidaderemoção de AuthService

--- src/app/core/services/auth.guard.ts
+++ src/app/core/services/auth.guard.ts
@@ -3,12 +3,15 @@ import { inject } from "@angular/core";
 import { Router } from "@angular/router";
 import { AuthFacade } from "../facades/auth.facade";
 
-export const authGuard: CanActivateFn = () => {
-    const authFacade = inject(AuthFacade);
-    const router = inject(Router);
 
-    if(!authFacade.usuarioLogado()){
-        return true;
-    }
-    return router.createUrlTree(['/login']);
+export const authGuard: CanActivateFn = () => {
+   const router= inject(Router);
+   const authFacade = inject(AuthFacade);
+
+   if(authFacade.usuarioLogado()){
+      return true;
+   }
+   return router.createUrlTree(['/login']);
+   
 }
+//SE O USUARIO TIVER AUTENTICADO O MESMO TERÁ ACESSO PARA A RORA
