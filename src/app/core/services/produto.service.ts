75055fcec889c418920965bd9c307b79b330cc2b
Davi Martins <davi15mmartins@gmail.com>
2026-08-21 17:10:47 -0300

    refactor(produto.service.ts): melhora lógica do serviço de produtos

--- src/app/core/services/produto.service.ts
+++ src/app/core/services/produto.service.ts
@@ -1,14 +1,11 @@
 import { inject, Injectable } from "@angular/core"
 import { Inject } from "@angular/core"
 import { HttpClient } from "@angular/common/http"
+import { ProdutoLoja } from "../models/produto-loja";
 
 type ProdutoApi = {
     title: string;
     price: number;
-};
-type Produto = {
-    nome: string;
-    preco: number;
 };
 
 @Injectable({providedIn: 'root'})
@@ -16,15 +13,15 @@ export class produtoService {
 
     private http = inject(HttpClient);
 
-    private API = 'https://fakestoreapi.com/products';
+    private readonly API = 'https://fakestoreapi.com/products';
 
     buscarProdutos(){
         return this.http.get< ProdutoApi []>(this.API);
     }
-    transformarProdutos(dados: ProdutoApi []): Produto[] {
-        return dados.map((p) =>({
-            nome: p.title,
-            preco: p.price,
+    transformarProdutos(dados: ProdutoApi []): ProdutoLoja[] {
+        return dados.map((produto) =>({
+            nome: produto.title,
+            preco: produto.price,
         }));
     }
 }
